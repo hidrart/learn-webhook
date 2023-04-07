f2a5acff8429a74cddf75fee01af2d3ababf0b03
hidrart <hidayattulravli@gmail.com>
2023-04-07 16:15:10 +0700

    testing-webhook

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('This is Online!');
+});
+
+// testing webhooks from webhook.site
+app.get('/hook', (req, res) => {
+	res.send('This is a webhook!');
+});
+
+app.post('/hook', (req, res) => {
+	console.log(req.body);
+	res.send('This is a webhook!');
+});
+
+app.listen(port, () => {
+	console.log(`server running on port ${port}`);
+});
